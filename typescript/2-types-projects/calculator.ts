791e06599a8b0bfe9cb01c7f53fb36cbabdb1d46
Knoa0405 <mike1124@naver.com>
2021-12-02 23:26:32 +0900

    Update typescript

---
+++ typescript/2-types-projects/calculator.ts
@@ -0,0 +1,24 @@
+/**
+ * Let's make a calculator 🧮
+ */
+{
+    type ObjType = {
+        [type : string] : Function;
+    }
+    function calculate(type : string, ...numbers : number[]) : void {
+        const types : ObjType = {
+            'add' : () => numbers.reduce((acc,cur) => acc + cur),
+            'substract' : () => numbers.reduce((acc,cur) => acc - cur),
+            'multiply' : () => numbers.reduce((acc,cur) => acc * cur),
+            'divide' : () => numbers.reduce((acc,cur) => acc / cur),
+            'remainder' : () => numbers.reduce((acc,cur) => acc % cur),
+        };
+
+        return types[type]();
+    }
+    console.log(calculate('add', 1, 3)); // 4
+    console.log(calculate('substract', 3, 1)); // 2
+    console.log(calculate('multiply', 4, 2)); // 8
+    console.log(calculate('divide', 4, 2)); // 2
+    console.log(calculate('remainder', 5, 2)); // 1
+}
