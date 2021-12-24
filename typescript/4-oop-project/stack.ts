f7ea245df5e69ad026145a6ae21526ad22be5f83
Knoa <mike1124@naver.com>
2021-12-24 23:39:05 +0900

    Complete stack tyepscript practice

--- typescript/4-oop-project/stack.ts
+++ typescript/4-oop-project/stack.ts
@@ -3,79 +3,58 @@
     // 자바스크립트에서 제공하는 자료구조 사용하지 않기!
     interface Stack {
         readonly size: number;
-        head: number | null;
-        push(item: Item): void;
+        push(value : string): void;
         pop(): string;
     }
-    interface Link {
-        prevLink: number;
-        nextLink: number;
-    }
-    interface Item extends Link {
-        text: string;
+
+    type StackNode = {
+        readonly value: string;
+        readonly next?: StackNode;
     }
 
-    class ItemClass implements Item {
-        prevLink = 0;
-        text = '';
-        nextLink = 0;
-
-        constructor(value : string) {
-            this.prevLink = Date.now();
-            this.text = value;
-            this.nextLink = Date.now() + 10;
-            
-        }
-    }
     // 연결 리스트로 구현
     // pop or push 함수 입력할 때마다 저장된 값이 FIFO 원칙으로 나와야 한다.
-    class StackClass implements Stack {
-        size: number = 10; // 스택의 사이즈를 정함.
-        head: number | null = null; // 링크 리스트 head link 를 가리킴.
+    class StackImpl implements Stack {
+        private _size: number = 0; // 스택의 사이즈를 정함.
+        private head?: StackNode; // 링크 리스트 head link 를 가리킴.
 
-        temp: Item = {
-            prevLink: 0,
-            text: '',
-            nextLink: 0
-        };
+        get size(){
+            return this._size;
+        }
 
         stackOverflowError() {
-            if(this.size === 0) {
+            if(this.size >= 10) {
                 throw new Error('Stack overflow');
             }
         }
 
-        checkEmpty() {
-            if(this.head === null) {
-                throw new Error('Empty Stack');
-            }
-        }
-
-        push(item : Item): void {
+        push(value : string): void {
             this.stackOverflowError();
-            this.size += 1;
-            if(this.head === null) {
-                this.head = item.prevLink;
-                this.temp.text = item.text;
-            }
-            if(this.head !== null) {
-                item.prevLink = this.temp.nextLink;
-                this.temp.text = item.text;
-            }
-            this.temp.nextLink = item.nextLink;
+            const node: StackNode = { value, next: this.head };
+            this.head = node;
+            this._size ++;
         }
 
         pop(): string {
-            this.checkEmpty();
-            this.size -= 1;
-            this.head = this.temp.nextLink;
-            return this.temp.text;
+            if(this.head == null) {
+                throw new Error('Empty Stack');
+            }
+            const node: StackNode = this.head;
+            this.head = node.next;
+            this._size --;
+            return node.value;
         }
     }
-    const item = new ItemClass('HELLO');
-    const item2 = new ItemClass('HI');
 
-    const stack = new StackClass();
+    const stack = new StackImpl();
+
+    stack.push('ELLIE !');
+    stack.push('BOB 2');
+    stack.push('Steve 3');
     
-    stack.push(item);
+    while(stack.size !== 0) {
+        console.log(stack.pop());
+    }
+
+    stack.pop();
 }
