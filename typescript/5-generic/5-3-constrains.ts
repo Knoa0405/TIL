4375f1c047bdb6df504225820d726cb22f0756df
Knoa <mike1124@naver.com>
2021-12-26 20:01:48 +0900

    Update Typescript - generic constrains - 2

--- typescript/5-generic/5-3-constrains.ts
+++ typescript/5-generic/5-3-constrains.ts
@@ -39,4 +39,22 @@
 
     const ellieAfterPay = pay(ellie);
     const bobAfterPay = pay(bob);
+
+    const obj = {
+        name : 'ellie',
+        age: 20,
+    }
+    const obj2 = {
+        animal : 'dog',
+    }
+
+    function getValue<T,K extends keyof T>(obj : T, key: K): T[K] {
+        return obj[key];
+    }
+
+
+    console.log(getValue(obj, 'name')); // ellie
+    console.log(getValue(obj, 'age')); // 20
+    console.log(getValue(obj2, 'animal')); // dog
+
 }
