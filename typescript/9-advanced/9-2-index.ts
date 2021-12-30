73843b42b6a894d2e75cc325342789331131de9e
Knoa <mike1124@naver.com>
2021-12-30 23:42:28 +0900

    Update Typescript - index

---
+++ typescript/9-advanced/9-2-index.ts
@@ -0,0 +1,29 @@
+{
+    const obj = {
+        name : 'ellie',
+    };
+
+    obj.name;
+    obj['name'];
+
+    type Animal = {
+        name : string;
+        age : number;
+        gender : 'male' | 'female';
+    }
+
+    type Name = Animal['name']
+
+    const text:Name = 'hello';
+
+    type Gender = Animal['gender']; // 'male' | 'female'
+
+    type Keys = keyof Animal; // "name" | "age" | "gender"
+
+    const key: Keys = 'name';
+
+    type Peson = {
+        name : string;
+        gender: Animal['gender'];
+    }
+}
